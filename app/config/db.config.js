53b1fa8d32620e93e55849b312a241053712f05a
slby99 <slby99@gmail.com>
2022-08-14 16:01:10 +0800

    2nd commit

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,5 +2,5 @@ require('dotenv').config();
 
 module.exports = {
     //url: "mongodb+srv://slby99:slby99@cluster0.bgg9ug7.mongodb.net/?retryWrites=true&w=majority"
-    url: process.env.DATABASE
+    url: process.env.MONGODB_URI
   };
--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.get("/", (req, res) => {
 
 require("./app/routes/history.routes")(app);
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
